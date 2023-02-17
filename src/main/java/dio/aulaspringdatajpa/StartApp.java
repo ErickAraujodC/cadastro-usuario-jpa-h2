af7bb3f28df4482ed08f5c41ddffe0c13f366e72
ErickAraujodC <erickbyborges@gmail.com>
2023-02-17 00:11:02 -0300

    first commit

---
+++ src/main/java/dio/aulaspringdatajpa/StartApp.java
@@ -0,0 +1,40 @@
+package dio.aulaspringdatajpa;
+
+import dio.aulaspringdatajpa.model.User;
+import dio.aulaspringdatajpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class StartApp implements CommandLineRunner {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public void run(String... args) throws Exception {
+        List<User> users = repository.findByNameContaining("Joao");
+        for (User u: users) {
+            System.out.println(u);
+        }
+
+        insertUser("Erick", "ErickCB", "123cb");
+        insertUser("Gislaine", "GiAesthetic", "jabuti123");
+        insertUser("Osvaldo", "PorcoOsvaldo", "suino123");
+        insertUser("Poou", "kungFuPanda", "mestre123");
+
+        for (User u: repository.findAll()) {
+            System.out.println(u);
+        }
+    }
+    private void insertUser(String name, String userName, String password){
+        User user = new User();
+        user.setName(name);
+        user.setUserName(userName);
+        user.setPassword(password);
+        repository.save(user);
+
+    }
+
+}
